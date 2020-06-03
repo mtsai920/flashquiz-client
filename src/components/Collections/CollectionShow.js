386547b928cb592aa5e64ff6689e5c6f53df5a92
mtsai <megantsai920@gmail.com>
2020-06-02 22:07:14 -0400

    Added delete to CollectionShow - 1. Continued working on CollectionShow.js 2. Added delete button with Redirect 3. Added Axios calls when page is rendered

--- src/components/Collections/CollectionShow.js
+++ src/components/Collections/CollectionShow.js
@@ -1,26 +1,70 @@
-import React from 'react'
-import { withRouter } from 'react-router-dom'
-// import axios from 'axios'
+import React, { useState, useEffect } from 'react'
+import { withRouter, Redirect } from 'react-router-dom'
+import apiUrl from '../../apiConfig'
+import axios from 'axios'
 
 const CollectionShow = (props) => {
-  // const [collection, setCollection] = useState({
-  //   title: null,
-  //   description: null,
-  //   cards: []
-  // })
+  const [collection, setCollection] = useState({
+    title: null,
+    description: null,
+    cards: []
+  })
 
-  // useEffect(() => {
-  //   axios({
-  //     url: `${apiUrl}/collections/${match.params._id}`
-  //   })
-  // })
+  const [deleted, setDeleted] = useState(false)
 
-  console.log(props)
+  const { match, user, msgAlert } = props
+
+  useEffect(() => {
+    axios({
+      url: `${apiUrl}/collections/${match.params.id}`,
+      method: 'GET',
+      headers: {
+        'Authorization': `Token token=${user.token}`
+      }
+    })
+      .then(res => setCollection(res.data.collection))
+      .catch((err) => {
+        msgAlert({
+          heading: 'Uh oh!',
+          variant: 'danger',
+          message: 'Failed to retrieve collection due to ' + err.message
+        })
+      })
+  }, [])
+
+  const destroy = () => {
+    axios({
+      url: `${apiUrl}/collections/${match.params.id}`,
+      method: 'DELETE',
+      headers: {
+        'Authorization': `Token token=${user.token}`
+      }
+    })
+      .then(() => {
+        msgAlert({
+          heading: 'Success!',
+          variant: 'success',
+          message: 'Collection successfully deleted.'
+        })
+      })
+      .then(() => setDeleted(true))
+  }
+
+  if (!collection) {
+    return (
+      <div>
+        <h2>Loading...</h2>
+      </div>
+    )
+  } else if (deleted) {
+    return <Redirect to={'/collections'} />
+  }
 
   return (
     <div>
-      <h1>Collection Show</h1>
-      <button>Delete</button>
+      <h1>{collection.title}</h1>
+      <h2>{collection.description}</h2>
+      <button onClick={destroy}>Delete</button>
     </div>
   )
 }
