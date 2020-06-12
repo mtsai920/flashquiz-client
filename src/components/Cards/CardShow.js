29d7aa224c3690d63d73bccfb14e6135fa319189
mtsai <megantsai920@gmail.com>
2020-06-12 10:34:25 -0400

    Added delete call and used Form in CardShow - 1. Added delete API call 2. Added Redirect back to collections page when deleted 3. Auto-populate form with card info in CardShow

--- src/components/Cards/CardShow.js
+++ src/components/Cards/CardShow.js
@@ -1,18 +1,22 @@
 import React, { useEffect, useState } from 'react'
-import { withRouter } from 'react-router-dom'
+import { withRouter, Redirect } from 'react-router-dom'
 import axios from 'axios'
 import apiUrl from '../../apiConfig'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
 
 const CardShow = (props) => {
   console.log(props)
-
-  const { match, user } = props
 
   const [card, setCard] = useState({
     term: '',
     definition: '',
     collectionId: ''
   })
+
+  const [deleted, setDeleted] = useState(null)
+
+  const { match, user, location, msgAlert } = props
 
   useEffect(() => {
     axios({
@@ -26,6 +30,29 @@ const CardShow = (props) => {
       .catch(console.err)
   }, [])
 
+  const destroy = event => {
+    axios({
+      url: `${apiUrl}/cards/${match.params.id}`,
+      method: 'DELETE',
+      headers: {
+        'Authorization': `Token token=${user.token}`
+      }
+    })
+      .then(() => setDeleted(true))
+      .then(() => {
+        msgAlert({
+          heading: 'Success!',
+          variant: 'success',
+          message: 'Card has been deleted.'
+        })
+      })
+      .catch(console.err)
+  }
+
+  if (deleted) {
+    return <Redirect to={`/collections/${location.card.collectionId}`}/>
+  }
+
   if (!card) {
     return (
       <div>
@@ -36,8 +63,29 @@ const CardShow = (props) => {
 
   return (
     <div>
-      <h2>{card.term}</h2>
-      <h3>{card.definition}</h3>
+      <h2>Update your Card</h2>
+      <Form>
+        <Form.Group controlId="formGroupEmail">
+          <Form.Label>Term</Form.Label>
+          <Form.Control
+            type="text"
+            name="term"
+            value={card.term}
+            // onChange={handleChange}
+            required />
+        </Form.Group>
+        <Form.Group controlId="exampleForm.ControlTextarea1">
+          <Form.Label>Definition</Form.Label>
+          <Form.Control as="textarea" rows="3"
+            maxLength="350"
+            name="definition"
+            value={card.definition}
+            // onChange={handleChange}
+            required />
+        </Form.Group>
+        <Button type="submit">Update Card</Button>
+        <Button onClick={destroy}>Delete</Button>
+      </Form>
     </div>
   )
 }
